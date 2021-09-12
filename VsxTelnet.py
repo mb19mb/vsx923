b8f2b5a406ef56f22fc9b4d38d4483bdc9666107
Matthias Beyer <m.beyer@axilaris.de>
2021-09-12 11:19:39 +0200

    telnet

--- VsxTelnet.py
+++ VsxTelnet.py
@@ -6,9 +6,10 @@ HOST = "192.168.20.208"
 PORT = 8102
 
 tn = telnetlib.Telnet(None)
+tn.set_debuglevel(5)
 tn.open(HOST, PORT)
-#out = tn.read_until("VOL".encode('ascii'), 20)
-out = tn.read_all()
+out = tn.read_until("VOL".encode('ascii'), 20)
+#out = tn.read_all()
 tn.write("110VL".encode('ascii') + "\r\n".encode('ascii'))
 tn.close()
 print(out)
