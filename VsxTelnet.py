9e253812c93f828f9c81aafe56060c007d60f023
Matthias Beyer <m.beyer@axilaris.de>
2021-09-12 11:12:05 +0200

    telnet

---
+++ VsxTelnet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import getpass
+import telnetlib
+
+HOST = "192.168.20.208"
+PORT = 8192
+
+tn = telnetlib.Telnet(None)
+tn.open(HOST, PORT)
+out = tn.read_until("VOL".encode('ascii'), 20)
+tn.write("110VL".encode('ascii') + "\r\n".encode('ascii'))
+print(out)
+tn.close()
